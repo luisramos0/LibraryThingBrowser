9279bb0491d3911e29d27f806c8c62ee53bcf3e5
nietky <nietky2@gmail.com>
2012-12-31 18:34:37 +1030

    Now you can share "VIEW" XLS attachment in Gmail and choose LibraryThing Browser as an option. That will load the ImportData activity with an intent and the data file attached.

--- gen/com/nietky/librarythingbrowser/R.java
+++ gen/com/nietky/librarythingbrowser/R.java
@@ -36,43 +36,49 @@ public final class R {
         public static final int book_list=0x7f090001;
         public static final int book_list_item_subtitle=0x7f090003;
         public static final int book_list_item_title=0x7f090002;
-        public static final int menuSearch=0x7f09000a;
-        public static final int menu_preferences=0x7f09000b;
+        public static final int menuSearch=0x7f09000b;
+        public static final int menu_preferences=0x7f09000c;
+        public static final int menu_settings=0x7f09000a;
     }
     public static final class layout {
         public static final int activity_book_detail=0x7f030000;
         public static final int activity_book_list=0x7f030001;
         public static final int activity_book_twopane=0x7f030002;
-        public static final int book_list_item=0x7f030003;
-        public static final int fragment_book_detail=0x7f030004;
+        public static final int activity_import_data=0x7f030003;
+        public static final int book_list_item=0x7f030004;
+        public static final int fragment_book_detail=0x7f030005;
     }
     public static final class menu {
-        public static final int options=0x7f080000;
+        public static final int activity_import_data=0x7f080000;
+        public static final int options=0x7f080001;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
-        public static final int pref_default_display_name=0x7f050008;
-        public static final int pref_description_social_recommendations=0x7f050006;
+        public static final int hello_world=0x7f050003;
+        public static final int menu_settings=0x7f050004;
+        public static final int pref_default_display_name=0x7f05000b;
+        public static final int pref_description_social_recommendations=0x7f050009;
         /**  Example settings for Data & Sync 
          */
-        public static final int pref_header_data_sync=0x7f05000a;
+        public static final int pref_header_data_sync=0x7f05000d;
         /**  Strings related to Settings 
  Example General settings 
          */
-        public static final int pref_header_general=0x7f050004;
+        public static final int pref_header_general=0x7f050007;
         /**  Example settings for Notifications 
          */
-        public static final int pref_header_notifications=0x7f05000d;
-        public static final int pref_ringtone_silent=0x7f050010;
-        public static final int pref_title_add_friends_to_messages=0x7f050009;
-        public static final int pref_title_display_name=0x7f050007;
-        public static final int pref_title_new_message_notifications=0x7f05000e;
-        public static final int pref_title_ringtone=0x7f05000f;
-        public static final int pref_title_social_recommendations=0x7f050005;
-        public static final int pref_title_sync_frequency=0x7f05000b;
-        public static final int pref_title_system_sync_settings=0x7f05000c;
-        public static final int pref_title_vibrate=0x7f050011;
-        public static final int title_activity_settings=0x7f050003;
+        public static final int pref_header_notifications=0x7f050010;
+        public static final int pref_ringtone_silent=0x7f050013;
+        public static final int pref_title_add_friends_to_messages=0x7f05000c;
+        public static final int pref_title_display_name=0x7f05000a;
+        public static final int pref_title_new_message_notifications=0x7f050011;
+        public static final int pref_title_ringtone=0x7f050012;
+        public static final int pref_title_social_recommendations=0x7f050008;
+        public static final int pref_title_sync_frequency=0x7f05000e;
+        public static final int pref_title_system_sync_settings=0x7f05000f;
+        public static final int pref_title_vibrate=0x7f050014;
+        public static final int title_activity_import_data=0x7f050005;
+        public static final int title_activity_settings=0x7f050006;
         public static final int title_book_detail=0x7f050001;
         public static final int title_book_list=0x7f050002;
     }
