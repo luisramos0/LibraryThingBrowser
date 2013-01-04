1e4597904cf56b6c4a62c57057eece29fd7a8312
nietky <nietky2@gmail.com>
2013-01-04 18:13:19 +1030

    Removed fragments

--- src/com/nietky/librarythingbrowser/BookDetailActivity.java
+++ src/com/nietky/librarythingbrowser/BookDetailActivity.java
@@ -1,42 +1,255 @@
 package com.nietky.librarythingbrowser;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.NavUtils;
-import android.view.MenuItem;
+import android.text.Html;
+import android.util.Log;
+import android.view.Display;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout.LayoutParams;
+import android.widget.TextView;
 
-public class BookDetailActivity extends FragmentActivity {
+public class BookDetailActivity extends Activity {
+    public static final String TAG = "BookDetailActivity";
 
+    private String id;
+    private Cursor cursor;
+    private Context context;
+
+    private HashMap<String, String> fields;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_detail);
-
-        // The list on the previous view is reset to the top, whereas if you hit the "Back" button it
-        // remains in the previous location.
         
-        // getActionBar().setDisplayHomeAsUpEnabled(true);
+        context = this;
+        
+        Intent intent = getIntent();
+        id = intent.getStringExtra("_id");
+        DbHelperNew dbHelper = new DbHelperNew(getApplicationContext());
+        dbHelper.open();
+        cursor = dbHelper.getRow(id);
+        cursor.moveToFirst();
+        Log.d(TAG, "getting Row id=" + id);
 
-        if (savedInstanceState == null) {
-            Bundle arguments = new Bundle();
-            arguments.putString(BookDetailFragment.ARG_ITEM_ID, getIntent()
-                    .getStringExtra(BookDetailFragment.ARG_ITEM_ID));
-            BookDetailFragment fragment = new BookDetailFragment();
-            fragment.setArguments(arguments);
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.book_detail_container, fragment).commit();
+        fields = new HashMap<String, String>();
+
+        String fieldname;
+        String[] fieldnames = { "_id", "book_id", "title", "author1",
+                "author2", "author_other", "publication", "date", "ISBNs",
+                "series", "source", "lang1", "lang2", "lang_orig", "LCC",
+                "DDC", "bookcrossing", "date_entered", "date_acquired",
+                "date_started", "date_ended", "stars", "collections", "tags",
+                "review", "summary", "comments", "comments_private", "copies",
+                "encoding" };
+        for (int i = 0; i < fieldnames.length; i += 1) {
+            fieldname = fieldnames[i];
+            Log.d(TAG, "Getting content for fieldname=" + fieldname);
+            int index = cursor.getColumnIndex(fieldname);
+            String content = "";
+            if (index > -1)
+                content = cursor.getString(index);
+            content = content.replace("[return]", "\n");
+            fields.put(fieldname, content);
         }
+        dbHelper.close();
+        
+        setTitle(fields.get("title"));
+
+        TextView title_view = (TextView) findViewById(R.id.book_detail_title);
+        title_view.setText(fields.get("title"));
+
+        TextView author_view = (TextView) findViewById(R.id.book_detail_author);
+        String author;
+        String author1 = fields.get("author1");
+        String author2 = fields.get("author2");
+        if (author2 == "") {
+            author = author1;
+        } else {
+            author = author2;
+        }
+        author_view.setText(author);
+        author_view.setTag(author);
+        author_view.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                TextView self = (TextView) v;
+                String author = (String) self.getTag();
+//                Toast.makeText(getActivity(), tagTagText, 10).show();
+                Intent resultIntent = new Intent(context,
+                        BookListActivity.class);
+                resultIntent.putExtra("author1Name", author);
+                startActivity(resultIntent);
+            }
+        });
+        
+
+        TextView publication_date_view = (TextView) findViewById(R.id.book_detail_publication_date);
+        String publication_details = fields.get("publication");
+        if (publication_details == "")
+            publication_details = fields.get("date");
+        publication_date_view.setText(publication_details);
+
+        LinearLayout ll = (LinearLayout) findViewById(R.id.book_detail_tags_container);
+        ArrayList<String> tagNames = new ArrayList<String>();
+        Collections.addAll(tagNames, fields.get("tags").split(","));
+
+        Display display = getWindowManager().getDefaultDisplay();
+        int maxWidth = display.getWidth() - 10;
+
+        if (tagNames.size() > 0) {
+            LinearLayout llAlso = new LinearLayout(context);
+            llAlso.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT,
+                    LayoutParams.WRAP_CONTENT));
+            llAlso.setOrientation(LinearLayout.HORIZONTAL);
+
+            TextView tvIntro = new TextView(context);
+            tvIntro.setText("Tags:");
+
+            llAlso.addView(tvIntro);
+            tvIntro.measure(0, 0);
+
+            int widthSoFar = tvIntro.getMeasuredWidth();
+            for (String tagText : tagNames) {
+                TextView tvTag = new TextView(context, null,
+                        android.R.attr.textColorLink);
+                tvTag.setText(tagText);
+                tvTag.setTextSize(14);
+                tvTag.setTextColor(Color.BLACK);
+                tvTag.setBackgroundResource(R.drawable.rounded_edges_tag);
+                tvTag.setTag(tagText);
+                tvTag.setOnClickListener(new OnClickListener() {
+                    public void onClick(View v) {
+                        TextView self = (TextView) v;
+                        String tagTagText = (String) self.getTag();
+//                        Toast.makeText(getActivity(), tagTagText, 10).show();
+                        Intent resultIntent = new Intent(context,
+                                BookListActivity.class);
+                        resultIntent.putExtra("tagName", tagTagText);
+                        startActivity(resultIntent);
+                    }
+                });
+
+                LinearLayout.LayoutParams llp = new LinearLayout.LayoutParams(
+                        LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+                int left_right = 5;
+                int top_bottom = 3;
+                llp.setMargins(left_right, top_bottom, left_right, top_bottom); // llp.setMargins(left,
+                                                                                // top,
+                                                                                // right,
+                                                                                // bottom);
+                tvTag.setLayoutParams(llp);
+
+                tvTag.measure(0, 0);
+                widthSoFar += tvTag.getMeasuredWidth() + left_right * 2;
+
+                if (widthSoFar >= maxWidth) {
+                    ll.addView(llAlso);
+
+                    llAlso = new LinearLayout(context);
+                    llAlso.setLayoutParams(new LayoutParams(
+                            LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
+                    llAlso.setOrientation(LinearLayout.HORIZONTAL);
+
+                    llAlso.addView(tvTag);
+                    widthSoFar = tvTag.getMeasuredWidth();
+                } else {
+
+                    llAlso.addView(tvTag);
+                }
+            }
+
+            ll.addView(llAlso);
+        }
+
+        
+        
+        
+        
+        LinearLayout ll2 = (LinearLayout) findViewById(R.id.book_detail_collections_container);
+        ArrayList<String> collectionNames = new ArrayList<String>();
+        Collections.addAll(collectionNames, fields.get("collections").split(","));
+
+        Display display2 = getWindowManager().getDefaultDisplay();
+        int maxWidth2 = display2.getWidth() - 10;
+
+        if (collectionNames.size() > 0) {
+            LinearLayout llAlso = new LinearLayout(context);
+            llAlso.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT,
+                    LayoutParams.WRAP_CONTENT));
+            llAlso.setOrientation(LinearLayout.HORIZONTAL);
+
+            TextView tvIntro = new TextView(context);
+            tvIntro.setText("Collections:");
+
+            llAlso.addView(tvIntro);
+            tvIntro.measure(0, 0);
+
+            int widthSoFar = tvIntro.getMeasuredWidth();
+            for (String collectionText : collectionNames) {
+                TextView tvTag = new TextView(context, null,
+                        android.R.attr.textColorLink);
+                tvTag.setText(collectionText);
+                tvTag.setTextSize(14);
+                tvTag.setTextColor(Color.BLACK);
+                tvTag.setBackgroundResource(R.drawable.rounded_edges_collection);
+                tvTag.setTag(collectionText);
+                tvTag.setOnClickListener(new OnClickListener() {
+                    public void onClick(View v) {
+                        TextView self = (TextView) v;
+                        String collectionTagText = (String) self.getTag();
+//                        Toast.makeText(getActivity(), collectionTagText, 10).show();
+                        Intent resultIntent = new Intent(context, BookListActivity.class);
+                        resultIntent.putExtra("collectionName", collectionTagText);
+                        startActivity(resultIntent);
+                    }
+                });
+
+                LinearLayout.LayoutParams llp = new LinearLayout.LayoutParams(
+                        LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+                int left_right = 5;
+                int top_bottom = 3;
+                llp.setMargins(left_right, top_bottom, left_right, top_bottom); // llp.setMargins(left,
+                                                                                // top,
+                                                                                // right,
+                                                                                // bottom);
+                tvTag.setLayoutParams(llp);
+
+                tvTag.measure(0, 0);
+                widthSoFar += tvTag.getMeasuredWidth() + left_right * 2;
+
+                if (widthSoFar >= maxWidth) {
+                    ll.addView(llAlso);
+
+                    llAlso = new LinearLayout(this);
+                    llAlso.setLayoutParams(new LayoutParams(
+                            LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
+                    llAlso.setOrientation(LinearLayout.HORIZONTAL);
+
+                    llAlso.addView(tvTag);
+                    widthSoFar = tvTag.getMeasuredWidth();
+                } else {
+
+                    llAlso.addView(tvTag);
+                }
+            }
+
+            ll2.addView(llAlso);
+        }
+        
+        TextView review_view = (TextView) findViewById(R.id.book_detail_review);
+        review_view.setText(Html.fromHtml("<p><b>Review:</b> "
+                + fields.get("review").replace("\n", "</p><p>")));
+
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            NavUtils.navigateUpTo(this,
-                    new Intent(this, BookListActivity.class));
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
--- src/com/nietky/librarythingbrowser/BookListActivity.java
+++ src/com/nietky/librarythingbrowser/BookListActivity.java
@@ -6,25 +6,42 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import android.app.AlertDialog;
+import android.app.ListActivity;
 import android.app.ProgressDialog;
 import android.app.SearchManager;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import android.preference.PreferenceManager;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.SearchView.OnQueryTextListener;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.googlecode.jcsv.CSVStrategy;
 import com.googlecode.jcsv.reader.CSVEntryParser;
 import com.googlecode.jcsv.reader.CSVReader;
 import com.googlecode.jcsv.reader.internal.CSVReaderBuilder;
 
-public class BookListActivity extends FragmentActivity implements
-        BookListFragment.Callbacks {
+public class BookListActivity extends ListActivity implements OnQueryTextListener {
 
     private boolean mTwoPane;
     private static Intent intent;
@@ -32,36 +49,122 @@ public class BookListActivity extends FragmentActivity implements
     private InputStreamReader inputStreamReader = null;
     private static final String TAG = "BookListActivity";
 
-    public BookListFragment getBookList() {
-        return ((BookListFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.book_list));
-    }
+    private static final String STATE_ACTIVATED_POSITION = "activated_position";
+    public static final int RESULT_TAG_SELECT = 1;
+    public static final int RESULT_COLLECTION_SELECT = 2;
+
+    private int mActivatedPosition = ListView.INVALID_POSITION;
+    private Cursor cursor;
+    private BookListCursorAdapter adapter;
+    public String searchFilter = "%";
+    public String tagFilter = "%";
+    public String collectionFilter = "%";
+    private String tableName = "books";
+
+    private SharedPreferences sharedPref;
+    private SharedPreferences.Editor prefsEdit;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_list);
-
-        if (findViewById(R.id.book_detail_container) != null) {
-            mTwoPane = true;
-            ((BookListFragment) getSupportFragmentManager().findFragmentById(
-                    R.id.book_list)).setActivateOnItemClick(true);
-        }
+        sharedPref = PreferenceManager.getDefaultSharedPreferences(this
+                .getApplicationContext());
 
         intent = getIntent();
         if (intent.getAction() == Intent.ACTION_VIEW) {
             importData();
         } else if (intent.getAction() == Intent.ACTION_SEARCH) {
             String query = intent.getStringExtra(SearchManager.QUERY);
-            getBookList().searchFilter = query;
-            getBookList().loadList();
+            searchFilter = query;
+            loadList();
         } else if (intent.hasExtra("tagName")) {
-            getBookList().loadTag(intent.getStringExtra("tagName"));
+            loadTag(intent.getStringExtra("tagName"));
         } else if (intent.hasExtra("collectionName")) {
-            getBookList().loadCollection(intent.getStringExtra("collectionName"));
+            loadCollection(intent.getStringExtra("collectionName"));
         } else if (intent.hasExtra("author1Name")) {
-            getBookList().loadAuthor(intent.getStringExtra("author1Name"));
+            loadAuthor(intent.getStringExtra("author1Name"));
+        } else {
+            loadList();
         }
+    }
+
+    public void loadList() {
+        DbHelperNew dbHelper = new DbHelperNew(this.getApplicationContext());
+        dbHelper.open();
+
+        String columnName = "title";
+        cursor = dbHelper.searchAllCols(searchFilter, columnName);
+        if (searchFilter != "%")
+            this.setTitle(searchFilter);
+        else
+            this.setTitle("All books");
+
+        dbHelper.close();
+
+        adapter = new BookListCursorAdapter(this, cursor);
+        setListAdapter(adapter);
+    }
+
+    public void loadTag(String tag) {
+        if (tag.contains("'")) {
+            Toast.makeText(
+                    this,
+                    "filtering by terms including apostrophes is not supported yet.. sorry!",
+                    7).show();
+            return;
+        }
+        DbHelperNew dbHelper = new DbHelperNew(this.getApplicationContext());
+        dbHelper.open();
+
+        tagFilter = tag;
+        String columnName = "title";
+        cursor = dbHelper.searchTag(tag, columnName);
+        this.setTitle(tag);
+        dbHelper.close();
+
+        adapter = new BookListCursorAdapter(this, cursor);
+        setListAdapter(adapter);
+    }
+
+    public void loadCollection(String collection) {
+        if (collection.contains("'")) {
+            Toast.makeText(
+                    this,
+                    "filtering by terms including apostrophes is not supported yet.. sorry!",
+                    7).show();
+            return;
+        }
+        DbHelperNew dbHelper = new DbHelperNew(this.getApplicationContext());
+        dbHelper.open();
+
+        String columnName = "title";
+        cursor = dbHelper.searchCollection(collection, columnName);
+        this.setTitle(collection);
+        dbHelper.close();
+
+        adapter = new BookListCursorAdapter(this, cursor);
+        setListAdapter(adapter);
+    }
+
+    public void loadAuthor(String author) {
+        if (author.contains("'")) {
+            Toast.makeText(
+                    this,
+                    "filtering by terms including apostrophes is not supported yet.. sorry!",
+                    7).show();
+            return;
+        }
+        DbHelperNew dbHelper = new DbHelperNew(this.getApplicationContext());
+        dbHelper.open();
+
+        String columnName = "author1";
+        cursor = dbHelper.searchAuthor(author, columnName);
+        this.setTitle(author);
+        dbHelper.close();
+
+        adapter = new BookListCursorAdapter(this, cursor);
+        setListAdapter(adapter);
     }
 
     public void importData() {
@@ -161,24 +264,142 @@ public class BookListActivity extends FragmentActivity implements
         @Override
         protected void onPostExecute(String result) {
             dialog.dismiss();
-            ((BookListFragment) getSupportFragmentManager().findFragmentById(
-                    R.id.book_list)).loadList();
+            loadList();
         }
     }
 
-    public void onItemSelected(String id) {
-        if (mTwoPane) {
-            Bundle arguments = new Bundle();
-            arguments.putString(BookDetailFragment.ARG_ITEM_ID, id);
-            BookDetailFragment fragment = new BookDetailFragment();
-            fragment.setArguments(arguments);
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.book_detail_container, fragment).commit();
+    @Override
+    public void onListItemClick(ListView listView, View view, int position,
+            long id) {
+        super.onListItemClick(listView, view, position, id);
+        cursor.moveToPosition(position);
+        String _id = cursor.getString(cursor.getColumnIndex("_id"));
+        Intent detailIntent = new Intent(this, BookDetailActivity.class);
+        detailIntent.putExtra("_id", _id);
+        startActivity(detailIntent);
+    }
 
-        } else {
-            Intent detailIntent = new Intent(this, BookDetailActivity.class);
-            detailIntent.putExtra(BookDetailFragment.ARG_ITEM_ID, id);
-            startActivityForResult(detailIntent, -1);
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        // Place an action bar item for searching.
+        this.getMenuInflater().inflate(R.menu.options, menu);
+        // MenuItem item = menu.add("Search");
+        MenuItem item = menu.findItem(R.id.menuSearch);
+        // item.setIcon(android.R.drawable.ic_menu_search);
+        // item.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+//        SearchView sv = new SearchView(this);
+//        sv.setOnQueryTextListener(this);
+//        item.setActionView(sv);
+        //
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case R.id.menuPreferences:
+            goToPreferences();
+            return true;
+        case R.id.menuSearch:
+            this.onSearchRequested();
+            return true;
+        case R.id.menuDelete:
+            AlertDialog show = new AlertDialog.Builder(this)
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .setTitle("Delete books")
+                    .setMessage(
+                            "Are you sure you want to delete all your books?\n\n(This is only for data imported into LibraryThing Browser on your device, and cannot affect your LibraryThing account).")
+                    .setPositiveButton("Yes",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog,
+                                        int which) {
+                                    String tableName = "books";
+                                    DbHelperNew dbHelper = new DbHelperNew(getApplicationContext());
+                                    dbHelper.delete();
+                                    loadList();
+                                }
+
+                            }).setNegativeButton("No", null).show();
+            return true;
+        case R.id.menuTags:
+            Intent intentTags = new Intent(this, TagListActivity.class);
+            intentTags.putExtra("searchFilter", searchFilter);
+            startActivity(intentTags);
+            return true;
+        case R.id.menuCollections:
+            Intent intentCollections = new Intent(this,
+                    CollectionListActivity.class);
+            intentCollections.putExtra("searchFilter", searchFilter);
+            ;
+            startActivity(intentCollections);
+            return true;
+        case R.id.menuAuthors:
+            Intent intentAuthors = new Intent(this, AuthorListActivity.class);
+            intentAuthors.putExtra("searchFilter", searchFilter);
+            startActivity(intentAuthors);
+            return true;
+        case R.id.menuImport:
+            Intent intent = new Intent(this, ImportActivity.class);
+            startActivity(intent);
+            return true;
+        default:
+            return false;
+        }
+    }
+
+    public void goToPreferences() {
+        Log.d("blf", "opening application settings");
+        Intent i = new Intent(this, SettingsActivity.class);
+        startActivity(i);
+    }
+
+    public boolean onQueryTextChange(String newText) {
+        // Called when the action bar search text has changed. Update
+        // the search filter, and restart the loader to do a new query
+        // with this filter.
+        if (newText.contains("'")) {
+            Toast.makeText(this,
+                    "search terms cannot includes apostrophes yet.. sorry!", 7)
+                    .show();
+            newText = newText.replace("'", "");
+        }
+        searchFilter = !TextUtils.isEmpty(newText) ? newText : "%";
+        loadList();
+        // getLoaderManager().restartLoader(0, null, this);
+        return true;
+    }
+
+    public boolean onQueryTextSubmit(String query) {
+        // Don't care about this.
+        return true;
+    }
+
+    public class BookListCursorAdapter extends CursorAdapter {
+        LayoutInflater inflater;
+
+        public BookListCursorAdapter(Context context, Cursor c) {
+            super(context, c);
+            inflater = LayoutInflater.from(context);
+        }
+
+        @Override
+        public void bindView(View view, Context context, Cursor cursor) {
+            TextView titleTV = (TextView) view
+                    .findViewById(R.id.book_list_item_title);
+            TextView subTitleTV = (TextView) view
+                    .findViewById(R.id.book_list_item_subtitle);
+            titleTV.setText(cursor.getString(cursor.getColumnIndex("title")));
+            subTitleTV.setText(cursor.getString(cursor
+                    .getColumnIndex("author2")));
+            // if
+            // (cursor.getString(cursor.getColumnIndex("tags")).contains("unread"))
+            // {
+            // view.setBackgroundColor(Color.GRAY);
+            // } else
+            // view.setBackgroundColor(Color.BLACK);
+        }
+
+        @Override
+        public View newView(Context context, Cursor cursor, ViewGroup parent) {
+            return inflater.inflate(R.layout.book_list_item, parent, false);
         }
     }
 }
